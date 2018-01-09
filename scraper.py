d0f420210a9d5921c3338ca6b9fb47b192f11617
Hamlet <hamlet.villa@gmail.com>
2018-01-08 22:38:05 -0500

    scraper extracts single data point, creates csv file

--- scraper.py
+++ scraper.py
@@ -3,12 +3,32 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+import csv
+from datetime import datetime
+
 # get url
 getpage = "http://www.bloomberg.com/quote/SPX:IND"
 
-# query website
+# get HTML of getpage url
 page = urllib2.urlopen(getpage)
 
 # parse html w/bs and store in var
 soup = BeautifulSoup(page, 'html.parser')
+
+
+# take out the <div> of name and get its value
+name_box = soup.find("h1", attrs = {"class": "name"})
+
+name = name_box.text.strip()
+print name
+
+# get index price
+price_box = soup.find("div", attrs = {"class":"price"})
+price = price_box.text
+print price
+
+# open csv file with append, old data not erased
+with open("index.csv", "a") as csv_file:
+  writer = csv.writer(csv_file)
+  writer.writerow([name, price, datetime.now()])
 
